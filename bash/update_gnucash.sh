4723abed13c16ecbce939706e1725220f7a5a64e
epistemik <epistemik@gmail.com>
2021-04-24 12:03:08 -0400

    updates

--- bash/update_gnucash.sh
+++ bash/update_gnucash.sh
@@ -2,7 +2,7 @@
 ULB_GNC=/usr/local/bak/gnc
 GNC_HOME=/home/marksa/Documents/Financial/Gnucash
 rm ${ULB_GNC}/HouseHold.gnucash.oldest
-cp -p ${ULB_GNC}/HouseHold.gnucash.older ${ULB_GNC}/oldest
+cp -p ${ULB_GNC}/HouseHold.gnucash.older ${ULB_GNC}/HouseHold.gnucash.oldest
 rm ${ULB_GNC}/HouseHold.gnucash.older
 cp -p ${ULB_GNC}/HouseHold.gnucash.old ${ULB_GNC}/HouseHold.gnucash.older
 rm ${ULB_GNC}/HouseHold.gnucash.old
@@ -12,13 +12,17 @@ cp -p ${ULB_GNC}/HouseHold.gnucash ${ULB_GNC}/HouseHold.gnucash.bak
 rm ${ULB_GNC}/HouseHold.gnucash
 cp -p ${GNC_HOME}/HouseHold.gnucash ${ULB_GNC}/HouseHold.gnucash
 PY_GNC_APPF=/newdata/dev/git/Python/Gnucash/app-files
-rm ${PY_GNC_APPF}/trial.gnc
-cp -p ${GNC_HOME}/HouseHold.gnucash ${PY_GNC_APPF}/trial.gnc
+rm ${PY_GNC_APPF}/hh5.gnc
+cp -p ${PY_GNC_APPF}/hh4.gnc ${PY_GNC_APPF}/hh5.gnc
+rm ${PY_GNC_APPF}/hh4.gnc
+cp -p ${PY_GNC_APPF}/hh3.gnc ${PY_GNC_APPF}/hh4.gnc
+rm ${PY_GNC_APPF}/hh3.gnc
+cp -p ${PY_GNC_APPF}/hh2.gnc ${PY_GNC_APPF}/hh3.gnc
+rm ${PY_GNC_APPF}/hh2.gnc
+cp -p ${PY_GNC_APPF}/hh1.gnc ${PY_GNC_APPF}/hh2.gnc
+rm ${PY_GNC_APPF}/hh1.gnc
+cp -p ${GNC_HOME}/HouseHold.gnucash ${PY_GNC_APPF}/hh1.gnc
 rm ${PY_GNC_APPF}/family.gnc
 cp -p ${GNC_HOME}/HouseHold.gnucash ${PY_GNC_APPF}/family.gnc
-rm ${PY_GNC_APPF}/main.gnc
-cp -p ${GNC_HOME}/HouseHold.gnucash ${PY_GNC_APPF}/main.gnc
-rm ${PY_GNC_APPF}/test.gnc
-cp -p ${GNC_HOME}/HouseHold.gnucash ${PY_GNC_APPF}/test.gnc
 # run from folder /newdata/dev/git/Python/Gnucash/app-files/
 ls -Alt |more
