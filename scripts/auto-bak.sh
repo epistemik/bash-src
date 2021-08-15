5f24d0f20be7605f0a8c29cae6a711b77ef4d479
epistemik <epistemik@gmail.com>
2021-08-15 18:26:50 -0400

    add comments; no need for the rm's if forcing the cp's :)

--- scripts/auto-bak.sh
+++ scripts/auto-bak.sh
@@ -6,14 +6,16 @@ MYHOME=/home/marksa
 HOME_GNC=${MYHOME}/Documents/Financial/Gnucash
 SEAGATE=${MYHOME}/Seagate/BACKUP/DIOMEDES/marksa
 
-# rsync folders
+# gnucash data
 rsync -aq ${HOME_GNC}/ /mhs2/FIN/Gnucash/
 rsync -aq ${HOME_GNC}/ ${SEAGATE}/Documents/Financial/Gnucash/
 
+# gnucash metadata
 HOME_DLS=${MYHOME}/.local/share
 rsync -aq ${HOME_DLS}/gnucash/ /mhs2/dot-local-share/gnucash/
 rsync -aq ${HOME_DLS}/gnucash/ ${SEAGATE}/dot-local-share/gnucash/
 
+# google info
 GIT_PYTHON=${MYHOME}/dev/git/Python
 GOOGLE=${GIT_PYTHON}/google
 rsync -aq ${GOOGLE}/ /mhs2/Google/
@@ -21,9 +23,7 @@ rsync -aq ${GOOGLE}/ ${SEAGATE}/git/Python/google/
 
 GNC_APPF=/home/marksa/dev/Gnucash/app-files
 # send any newer hh.gnc or hh.gnc.gcm to Google drive
-rm -f ${GNC_APPF}/hh.gnc
 cp -pf ${HOME_GNC}/HouseHold.gnucash ${GNC_APPF}/hh.gnc
-rm -f ${GNC_APPF}/hh.gnc.gcm
 cp -pf ${HOME_DLS}/gnucash/books/HouseHold.gnucash.gcm ${GNC_APPF}/hh.gnc.gcm
 
 for x in $(find /home/marksa/dev/Gnucash/app-files/ -type f -newer /home/marksa/dev/git/Python/google/auto-bak/ref-file -print); do cp $x /home/marksa/dev/git/Python/google/auto-bak/transfer/; done
