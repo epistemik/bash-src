8f07c553dc29449cfc3a1b1867041bb76afd4d1c
epistemik <epistemik@gmail.com>
2022-03-06 17:37:12 -0500

    keep track of file system disk space in a log file

---
+++ scripts/dfm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright (c) 2022 Mark Sattolo <epistemik@gmail.com>
+# keep track of file system disk space
+
+LOG_FILE=/home/marksa/dev/logs/dfm.log
+
+echo `date` >> $LOG_FILE
+df -m >> $LOG_FILE
