e85f64645119b99129e77c30184c4c766e15de81
epistemik <epistemik@gmail.com>
2024-01-21 13:43:22 -0500

    add some backups to Seagate; various changes to sources and destinations

--- scripts/auto-bak.sh
+++ scripts/auto-bak.sh
@@ -3,25 +3,28 @@
 # Copyright (c) 2022 Mark Sattolo <epistemik@gmail.com>
 
 #  rsync my home folder to the Samsung drive
-rsync -aq --exclude-from='/home/marksa/dev/rsync.exclude' /home/marksa/ /media/marksa/Samsung_T5/BACKUP/marksa/
-
-#  rsync important information to the backup partition and Seagate drive
-HOME_GNC=/home/marksa/Documents/Financial/Gnucash
+rsync -aq --exclude-from='/home/marksa/dev/rsync.samsung.exclude' /home/marksa/ /media/marksa/Samsung_T5/BACKUP/marksa/
+#  rsync my home folder to the Seagate drive
 SEAGATE=/home/marksa/Seagate/BACKUP/DIOMEDES/HOME
+#rsync -aq --exclude-from='/home/marksa/dev/rsync.seagate.exclude' /home/marksa/ ${SEAGATE}/
 
+#  rsync important information to the backup partition
+HOME_GNC=/home/marksa/Documents/Financial/Gnucash
 # gnucash data
 rsync -aq ${HOME_GNC}/ /mhs2/FIN/Gnucash/
 rsync -aq ${HOME_GNC}/ ${SEAGATE}/Documents/Financial/Gnucash/
 
-# gnucash metadata
+# gnucash / metadata
 HOME_DLS=/home/marksa/.local/share
 rsync -aq ${HOME_DLS}/gnucash/ /mhs2/dot-local-share/gnucash/
-rsync -aq ${HOME_DLS}/gnucash/ ${SEAGATE}/dot-local-share/gnucash/
+rsync -aq ${HOME_DLS}/ ${SEAGATE}/.local/share/
 
-# google secret info
-GOOGLE=/home/marksa/git/Python/google
-rsync -aq ${GOOGLE}/ /mhs2/Google/
-rsync -aq ${GOOGLE}/ ${SEAGATE}/git/Python/google/
+# python files
+PYTHON=/home/marksa/git/Python
+rsync -aq ${PYTHON}/ ${SEAGATE}/git/Python/
+
+# google dev files
+rsync -aq ${PYTHON}/google/ /mhs2/Google/
 
 # write a log record
 date >> /home/marksa/dev/logs/rsync.log
