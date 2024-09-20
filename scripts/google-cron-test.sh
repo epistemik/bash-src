35397ed0518b9c7b02f56f16b9088b40ed75f2de
epistemik <epistemik@gmail.com>
2024-09-20 12:56:27 -0400

    test adding a date string to the saved files

--- scripts/google-cron-test.sh
+++ scripts/google-cron-test.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
-# modified 2024-09-05
+# modified 2024-09-19
 
 # test 'while read; do; < find' as safer alternative to 'for in find; do' loop
 # see: https://github.com/koalaman/shellcheck/wiki/SC2044
 
-GNC_BAK=/home/marksa/dev/Gnucash/bak-files
+T7=/media/marksa/T7/BACKUP
+GNC_BAK=${T7}/dev/Gnucash/bak-files
+# identify each day's files
+today=$(date +%F)
+echo "${today}"
+cp -pf /home/marksa/Documents/Financial/Gnucash/HouseHold.gnucash "${GNC_BAK}/hh_${today}.gnc"
+cp -pf /home/marksa/.local/share/gnucash/books/HouseHold.gnucash.gcm "${GNC_BAK}/hh_gnc_${today}.gcm"
+
 GAB=/home/marksa/git/Python/google/auto-bak
 #for x in $(find ${GNC_BAK}/ -type f -newer ${GAB}/ref-file -print); do cp "$x" ${GAB}/transfer/; done
 
-DEST=/media/marksa/T7/BACKUP/test
+DEST=${T7}/test
 while IFS= read -r -d '' file
 do
   (( count++ ))
