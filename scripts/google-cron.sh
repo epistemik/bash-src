ce46338331e3731bb7c5a3be7c50e4dd6fa48dc9
epistemik <epistemik@gmail.com>
2024-09-20 15:19:42 -0400

    add a date string to the saved files names

--- scripts/google-cron.sh
+++ scripts/google-cron.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
-# modified 2024-09-05
+# modified 2024-09-19
 
 # send any newer HouseHold.gnucash or HouseHold.gnucash.gcm to Google drive
 
 GNC_BAK=/home/marksa/dev/Gnucash/bak-files
-cp -pf /home/marksa/Documents/Financial/Gnucash/HouseHold.gnucash ${GNC_BAK}/hh.gnc
-cp -pf /home/marksa/.local/share/gnucash/books/HouseHold.gnucash.gcm ${GNC_BAK}/hh.gnc.gcm
+# identify each day's files
+today=$(date +%F)
+#echo "${today}"
+cp -pf /home/marksa/Documents/Financial/Gnucash/HouseHold.gnucash "${GNC_BAK}/hh_${today}.gnc"
+cp -pf /home/marksa/.local/share/gnucash/books/HouseHold.gnucash.gcm "${GNC_BAK}/hh_gnc_${today}.gcm"
+#cp -pf /home/marksa/Documents/Financial/Gnucash/HouseHold.gnucash ${GNC_BAK}/hh.gnc
+#cp -pf /home/marksa/.local/share/gnucash/books/HouseHold.gnucash.gcm ${GNC_BAK}/hh.gnc.gcm
 
 GOOG=/home/marksa/git/Python/google
 GAB=${GOOG}/auto-bak
