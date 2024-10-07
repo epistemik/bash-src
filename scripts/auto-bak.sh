8faa9d3fec338666c8eeb886a4c617991d0b6160
epistemik <epistemik@gmail.com>
2024-10-07 12:25:07 -0400

    Seagate drive has died...? replace Seagate backups with SamsungT5 backups

--- scripts/auto-bak.sh
+++ scripts/auto-bak.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
-# modified 2024-09-28
+# modified 2024-10-06
 
 # DAILY backups with rsync
 RSYNC='rsync -aq'
 
-#  rsync my home folder to the Samsung T5
-${RSYNC} --exclude-from='/home/marksa/dev/rsync.samsung.exclude' /home/marksa/ /media/marksa/Samsung_T5/BACKUP/marksa/
-
 #  rsync important information to backup locations
 HOME_GNC=/home/marksa/Documents/Financial/Gnucash
 HOME_DLS=/home/marksa/.local/share
-SEAGATE=/home/marksa/Seagate
-SAMSUNGT7=/media/marksa/T7/BACKUP
+SAMSUNGT5=/samt5/BACKUP/marksa
+SAMSUNGT7=/samt7/BACKUP
+
+#  rsync my home folder to the Samsung T5
+${RSYNC} --exclude-from='/home/marksa/dev/rsync.samsung.exclude' /home/marksa/ ${SAMSUNGT5}
 
 # gnucash data
 ${RSYNC} ${HOME_GNC}/ /mhs2/FIN/Gnucash/
-${RSYNC} ${HOME_GNC}/ ${SEAGATE}/Documents/Financial/Gnucash/
+${RSYNC} ${HOME_GNC}/ ${SAMSUNGT5}/Documents/Financial/Gnucash/
 ${RSYNC} ${HOME_GNC}/ ${SAMSUNGT7}/Documents/Financial/Gnucash/
 # gnucash metadata
 ${RSYNC} ${HOME_DLS}/gnucash/ /mhs2/dot-local/share/gnucash/
@@ -25,16 +25,16 @@ ${RSYNC} /home/marksa/.config/gnucash/ /mhs2/dot-config/gnucash/
 #${RSYNC} /etc/gnucash/ /mhs2/etc-gnc/ # >> needs sudo
 
 # .local/share
-${RSYNC} --exclude-from='/home/marksa/dev/rsync.seagate.exclude' ${HOME_DLS}/ ${SEAGATE}/.local/share/
+${RSYNC} --exclude-from='/home/marksa/dev/rsync.samsung.exclude' ${HOME_DLS}/ ${SAMSUNGT5}/.local/share/
 ${RSYNC} ${HOME_DLS}/ ${SAMSUNGT7}/local/share/
 
 # python files
 DEV_PYTHON=/home/marksa/dev/Python
-${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ ${SEAGATE}/dev/Python/
+${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ ${SAMSUNGT5}/dev/Python/
 ${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ ${SAMSUNGT7}/dev/Python/
 ${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ /mhs2/dev-python/
 GIT_PYTHON=/home/marksa/dev/git/Python
-${RSYNC} ${GIT_PYTHON}/ ${SEAGATE}/git/Python/
+${RSYNC} ${GIT_PYTHON}/ ${SAMSUNGT5}/git/Python/
 ${RSYNC} ${GIT_PYTHON}/ ${SAMSUNGT7}/dev/git/Python/
 ${RSYNC} ${GIT_PYTHON}/ /mhs2/git-python/
 
