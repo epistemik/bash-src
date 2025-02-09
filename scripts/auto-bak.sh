b16803f69f2b3cffe5bc116137068c0d8f2e7338
epistemik <epistemik@gmail.com>
2025-02-09 14:22:39 -0500

    remove redundant T5 operations; add confing and opt gnucash saves for T7

--- scripts/auto-bak.sh
+++ scripts/auto-bak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
-# modified 2024-10-06
+# Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
+# modified 2025-02-08
 
 # DAILY backups with rsync
 RSYNC='rsync -aq'
@@ -17,27 +17,25 @@ ${RSYNC} --exclude-from='/home/marksa/dev/rsync.samsung.exclude' /home/marksa/ $
 
 # gnucash data
 ${RSYNC} ${HOME_GNC}/ /mhs2/FIN/Gnucash/
-${RSYNC} ${HOME_GNC}/ ${SAMSUNGT5}/Documents/Financial/Gnucash/
 ${RSYNC} ${HOME_GNC}/ ${SAMSUNGT7}/Documents/Financial/Gnucash/
 ${RSYNC} /home/marksa/dev/Gnucash/ ${SAMSUNGT7}/dev/Gnucash/
 # gnucash metadata
 ${RSYNC} ${HOME_DLS}/gnucash/ /mhs2/dot-local/share/gnucash/
 ${RSYNC} /home/marksa/.config/gnucash/ /mhs2/dot-config/gnucash/
+${RSYNC} /home/marksa/.config/gnucash/ ${SAMSUNGT7}/config/gnucash/
 ${RSYNC} /home/marksa/opt/etc/gnucash/ /mhs2/opt/etc/gnucash/
+${RSYNC} /home/marksa/opt/etc/gnucash/ ${SAMSUNGT7}/opt/etc/gnucash/
 
 # .local/share
-${RSYNC} --exclude-from='/home/marksa/dev/rsync.samsung.exclude' ${HOME_DLS}/ ${SAMSUNGT5}/.local/share/
 ${RSYNC} ${HOME_DLS}/ ${SAMSUNGT7}/local/share/
 
 # python files
 DEV_PYTHON=/home/marksa/dev/Python
-${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ ${SAMSUNGT5}/dev/Python/
-${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ ${SAMSUNGT7}/dev/Python/
 ${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ /mhs2/dev-python/
+${RSYNC} -f'- /VENV/' ${DEV_PYTHON}/ ${SAMSUNGT7}/dev/Python/
 GIT_PYTHON=/home/marksa/dev/git/Python
-${RSYNC} ${GIT_PYTHON}/ ${SAMSUNGT5}/git/Python/
-${RSYNC} ${GIT_PYTHON}/ ${SAMSUNGT7}/dev/git/Python/
 ${RSYNC} ${GIT_PYTHON}/ /mhs2/git-python/
+${RSYNC} ${GIT_PYTHON}/ ${SAMSUNGT7}/dev/git/Python/
 
 # write a log record
 echo "auto-bak finish @ $(date)" >> /home/marksa/dev/logs/rsync.log
