cfaaaf6897b45a1e89cc5b7e1416847df90d958c
epistemik <epistemik@gmail.com>
2025-12-10 10:31:02 -0500

    comment out actual transfer to google drive which doesn't work anymore; write to a google-cron log

--- scripts/google-cron.sh
+++ scripts/google-cron.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
-# modified 2024-09-21
+# Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
+# modified 2025-12-08
 
-# send any newer HouseHold.gnucash or HouseHold.gnucash.gcm to Google drive
+# find any newer HouseHold.gnucash or HouseHold.gnucash.gcm 
+# No longer able to send to Google drive @ Nov 26, 2025 with Mint 22 update
 
 GNC_BAK=/home/marksa/dev/Gnucash/bak-files
+
 # identify each day's files
 today=$(date +%F)
 cp -pf /home/marksa/Documents/Financial/Gnucash/HouseHold.gnucash "${GNC_BAK}/hh_${today}.gnc"
@@ -19,12 +21,15 @@ find ${GNC_BAK} -type f -newer ${GAB}/ref-file -exec cp {} ${GAB}/transfer/ \;
 find ${GNC_BAK} -name "hh*" -type f -mtime +2 -delete
 
 # send to my Google drive
-source "/home/marksa/dev/Python/VENV/venv311/bin/activate"
-python ${GOOG}/drive/driveAccess.py -s ${GAB}/transfer -p gnucash -l /home/marksa/dev/logs
-deactivate
+#source "/home/marksa/dev/Python/VENV/venv311/bin/activate"
+#python ${GOOG}/drive/driveAccess.py -s ${GAB}/transfer -p gnucash -l /home/marksa/dev/logs
+#deactivate
 
 mv -f ${GAB}/transfer/* ${GAB}/sent/
 touch ${GAB}/ref-file
 
 # clean up the 'sent' folder to only hold last 30 days worth of files
 find ${GAB}/sent -name "hh*" -type f -mtime +30 -delete
+
+# log finish
+echo "google-cron finish @ $(date)" >> /home/marksa/dev/logs/google-cron.log
